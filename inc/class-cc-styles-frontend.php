85ffc0f0e20daaccb95c55201d527d00884d82d3
Roy Mahfooz <mahfooz.pucit08@gmail.com>
2025-05-08 17:41:15 +0500

    more

--- inc/class-cc-styles-frontend.php
+++ inc/class-cc-styles-frontend.php
@@ -1,4 +1,9 @@
 <?php
+/**
+ * Frontend applica Woo CC Styles
+ *
+ */
+
 defined('ABSPATH') || exit;
 
 class Woo_CC_Styles_Frontend {
@@ -20,7 +25,6 @@ class Woo_CC_Styles_Frontend {
 		$this->style_options = get_option( 'woo_cc_styles', [] );
 		$this->style_fields  = CC_Styles_Options::get_fields();
 		$this->style_engine  = new CC_Styles_Appearance_Engine( $this->style_fields, $this->style_options );
-		
 		$this->init_hooks();
 	}
 
